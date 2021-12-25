e108606d5c9e924974c0ab25f3ade9c339c685ee
seonhyeokjun <tjsrmatn@naver.com>
2021-12-25 22:39:39 +0900

    채팅방 수정

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,23 +2,7 @@
   <div id="Chat">
     <main-header></main-header>
     <v-container>
-      유저이름:
-      <input
-          v-model="userName"
-          type="text"
-      >
-      내용: <input
-        v-model="message"
-        type="text"
-        @keyup="sendMessage"
-    >
-      <div
-          v-for="(item, idx) in recvList"
-          :key="idx"
-      >
-        <h3>유저이름: {{ item.userName }}</h3>
-        <h3>내용: {{ item.content }}</h3>
-      </div>
+
     </v-container>
     <main-footer></main-footer>
   </div>
@@ -37,58 +21,20 @@ export default {
     'main-footer' : MainFooter
   },
   created() {
-    this.connect();
+    this.$http.get('/chat/room').then((res) => {
+      this.rooms = res.data;
+    });
   },
   data(){
     return {
-      userName: "",
-      message: "",
-      recvList: []
+      rooms: []
     }
   },
   methods:{
-    sendMessage(){
-      if(e.keyCode === 13 && this.userName !== '' && this.message !== ''){
-        this.send()
-        this.message = ''
-      }
-    },
-    send(){
-      console.log("Send message:" + this.message);
-      if (this.stompClient && this.stompClient.connected) {
-        const msg = {
-          userName: this.userName,
-          content: this.message
-        };
-        this.stompClient.send("/receive", JSON.stringify(msg), {});
-      }
-    },
-    connect(){
-      const serverURL = "http://localhost:8080"
-      let socket = new SockJs(serverURL);
-      this.stompClient = Stomp.over(socket);
-      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
-      this.stompClient.connect(
-          {},
-          frame => {
-            // 소켓 연결 성공
-            this.connected = true;
-            console.log('소켓 연결 성공', frame);
-            // 서버의 메시지 전송 endpoint를 구독합니다.
-            // 이런형태를 pub sub 구조라고 합니다.
-            this.stompClient.subscribe("/send", res => {
-              console.log('구독으로 받은 메시지 입니다.', res.body);
+    createRoom(){
+      this.$http.post('/chat/room').then(() => {
 
-              // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
-              this.recvList.push(JSON.parse(res.body))
-            });
-          },
-          error => {
-            // 소켓 연결 실패
-            console.log('소켓 연결 실패', error);
-            this.connected = false;
-          }
-      );
+      });
     }
   }
 }
